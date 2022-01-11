f443accb4b19efc5c4d796e87b63283ef4ef9483
sugih2 <sugihnurdiansyah07.com>
2022-01-11 16:25:17 +0700

    CRUD acan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,29 @@
                 <div class="card-header">Data Kain</div>
 
                 <div class="card-body">
-<img src="{{ ('img/kain.jpg') }}" class="img-fluid" alt="...">
-                   
+                    <h1> Data Pegawai </h1>
                 </div>
+                <table border="1">
+                    <tr>
+                        <th>Nama</th>
+                        <th>Jabatan</th>
+                        <th>Umur</th>
+                        <th>Alamat</th>
+                        <th>Opsi</th>
+                    </tr>
+                    @foreach($pegawai as $p)
+                    <tr>
+                        <td> {{ $p->pegawai_nama }} </td>
+                        <td> {{ $p->pegawai_jabatan }} </td>
+                        <td> {{ $p->pegawai_umur }} </td>
+                        <td> {{ $p->pegawai_alamat }} </td>
+                        <td>
+                            <a href="/pegawai/edit/{{ $p->pegawai_id}}">Edit</a>
+                            <a href="/pegawai/hapus/{{ $p->pegawai_id}}">Hapus</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
             </div>
         </div>
     </div>
